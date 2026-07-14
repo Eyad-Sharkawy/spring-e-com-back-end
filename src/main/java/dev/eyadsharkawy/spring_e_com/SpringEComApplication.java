e7f542aa3f505b5e262d4782bb7db044f76a3682
Eyad-Sharkawy <eyad.m.sharkawy@gmail.com>
2026-07-14 18:16:10 +0300

    initial commit

---
+++ src/main/java/dev/eyadsharkawy/spring_e_com/SpringEComApplication.java
@@ -0,0 +1,13 @@
+package dev.eyadsharkawy.spring_e_com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEComApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEComApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/eyadsharkawy/spring_e_com/SpringEComApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.eyadsharkawy.spring_e_com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEComApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
